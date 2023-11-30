2437324c7d17da38ff9d1c105c4daf43672bbe1b
miveh <miveh@imivechi@gmail.com>
2023-11-30 23:55:57 +0330

    Customer layout component files added.

---
+++ src/components/layout/customer-layout/CustomerHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CustomerHeader() {
+  return <div>CustomerHeader</div>;
+}
+
+export default CustomerHeader;
---
+++ src/components/layout/customer-layout/CustomerLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CustomerLayout() {
+  return <div>CustomerLayout</div>;
+}
+
+export default CustomerLayout;
