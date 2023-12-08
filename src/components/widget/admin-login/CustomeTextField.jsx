c306db1004319b06455f8770dfe7585b9f7f1fd4
miveh <miveh@imivechi@gmail.com>
2023-12-08 13:13:47 +0330

    remove extra import

--- src/components/widget/admin-login/CustomeTextField.jsx
+++ src/components/widget/admin-login/CustomeTextField.jsx
@@ -1,6 +1,5 @@
 import { TextField } from "@mui/material";
 import React from "react";
-import { Field } from "formik";
 
 export function CustomeTextField({ id, label, name, autoComplete, ...rest }) {
   return (
